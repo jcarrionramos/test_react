b5d0ab6e331d5329ff506ed38320e4baaeb24eb1
Javier Carrion <jcarrion@cloner.cl>
2017-01-21 20:50:15 -0300

    fixing some bug

--- app/src/Page/CenterForm/CenterForm.js
+++ app/src/Page/CenterForm/CenterForm.js
@@ -11,40 +11,65 @@ import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 import DatePicker from 'react-bootstrap-date-picker';
 import {Treebeard} from 'react-treebeard';
-import './request.js';
 import './CenterForm.scss';
 
-const data = {
+function GETTunnel(url, auth, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', encodeURI(url));
+    xhr.setRequestHeader('Cloner_key', auth);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        success(JSON.parse(xhr.responseText));
+      } else {
+        error(xhr);
+      }
+    };
+    xhr.send();
+}
+
+var arbolRaiz = {
     name: 'root',
-    toggled: true,
-    children: [
-        {
-            name: 'parent',
-            children: [
-                { name: 'child1', children: [ { name: 'holyshit' } ]
-                },
-                { name: 'child2' }
-            ]
-        },
-        {
-            name: 'loading parent',
-            loading: true,
-            children: []
-        },
-        {
-            name: 'parent',
-            children: [
-                {
-                    name: 'nested parent',
-                    children: [
-                        { name: 'nested child 1' },
-                        { name: 'nested child 2' }
-                    ]
-                }
-            ]
-        }
-    ]
+    children: []
 };
+
+var undoneRequests = 1;
+function appendTree(tree) {
+  return function(children) {
+    for (var i = 0; i < children.length; i++){
+      LoadTree(children[i], tree);
+    }
+    undoneRequests = undoneRequests -1;
+    checkFinished(tree);
+  }
+}
+
+function LoadTree(child, tree) {
+  var treeChild = { name: child.Name }
+  tree.children.push(treeChild);
+
+  if (child.Type === 1){
+    return;
+  }
+
+  treeChild.children = []
+  undoneRequests = undoneRequests + 1
+  GETTunnel('https://zifre.cloner.cl/api/children?id='+ child.Id +'&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
+                           '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
+                           appendTree(treeChild),
+                           console.log);
+}
+
+function checkFinished(tree) {
+  if (undoneRequests === 0) {
+    console.log(arbolRaiz);
+  }
+}
+
+GETTunnel('https://zifre.cloner.cl/api/children?id=c506515ff13c4893ab4d7eac28a1c6e6343e72088a9d06bbb6f7c911117b55851747c50643391cf56013edd35a8cea89fa23d98a492c23da859f7252645b063f&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
+                         '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
+                         appendTree(arbolRaiz),
+                         console.log);
+
 
 class Tree extends React.Component {
     constructor(props){
@@ -61,10 +86,7 @@ class Tree extends React.Component {
     render(){
         return (
             <Treebeard
-                data={GETTunnel('https://zifre.cloner.cl/api/children?id=c506515ff13c4893ab4d7eac28a1c6e6343e72088a9d06bbb6f7c911117b55851747c50643391cf56013edd35a8cea89fa23d98a492c23da859f7252645b063f&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
-                                         '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
-                                         appendTree(arbolRaiz),
-                                         console.log)}
+                data={arbolRaiz}
                 onToggle={this.onToggle}
             />
         );
--- app/src/Page/CenterForm/request.js
+++ app/src/Page/CenterForm/request.js
@@ -18,7 +18,6 @@ var arbolRaiz = {
 };
 
 var undoneRequests = 1;
-
 function appendTree(tree) {
   return function(children) {
     for (var i = 0; i < children.length; i++){
@@ -38,7 +37,6 @@ function LoadTree(child, tree) {
   }
 
   treeChild.children = []
-
   undoneRequests = undoneRequests + 1
   GETTunnel('https://zifre.cloner.cl/api/children?id='+ child.Id +'&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
                            '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
@@ -46,13 +44,13 @@ function LoadTree(child, tree) {
                            console.log);
 }
 
-GETTunnel('https://zifre.cloner.cl/api/children?id=c506515ff13c4893ab4d7eac28a1c6e6343e72088a9d06bbb6f7c911117b55851747c50643391cf56013edd35a8cea89fa23d98a492c23da859f7252645b063f&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
-                         '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
-                         appendTree(arbolRaiz),
-                         console.log);
-
 function checkFinished(tree) {
   if (undoneRequests === 0) {
     console.log(arbolRaiz);
   }
 }
+
+GETTunnel('https://zifre.cloner.cl/api/children?id=c506515ff13c4893ab4d7eac28a1c6e6343e72088a9d06bbb6f7c911117b55851747c50643391cf56013edd35a8cea89fa23d98a492c23da859f7252645b063f&version=99999999&repo_id=51b7d8f0c0541ff5d6679e5be86a1b162038840e4149633b581431b4d4f44e183982238a81cf94d2da2ce4498204beb8c622886c099629a13db780821d81bdbc',
+                         '6f9536809658ceda12ae071783417d656e997409c83254dc85d391516abb0d7d66069bc77f68a0d30d77e261332a7e09429d96cd7909fbc00e46fef005d8737f',
+                         appendTree(arbolRaiz),
+                         console.log);
